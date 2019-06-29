e7979f4fa31a03b829c4ee654cf606ffdb158e13
KasiaKoper <katarzynakoper2@gmail.com>
2019-06-29 13:09:14 +0200

    repositories & service added

--- src/main/java/com/example/securitydatabase/repository/RoleRepository.java
+++ src/main/java/com/example/securitydatabase/repository/RoleRepository.java
@@ -1,4 +1,13 @@
 package com.example.securitydatabase.repository;
 
-public interface RoleRepository {
+import com.example.securitydatabase.model.Role;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//@Primary
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+    Role findByRole(String role);
+
 }
--- src/main/java/com/example/securitydatabase/repository/UserRepository.java
+++ src/main/java/com/example/securitydatabase/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package com.example.securitydatabase.repository;
 
-public class UserRepository {
+import com.example.securitydatabase.model.User;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//@Primary
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    User findByEmail(String email);     //this automatically implements a method
+
 }
